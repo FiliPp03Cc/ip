e5838ad785390589dc6a7fc76360571fa76b74ab
Filip <filip.caracoancea@emanuel.ro>
2024-03-04 19:20:16 +0100

    dulap

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	Sertar sertar =  new Sertar(1, 2, 3);
+	sertar.tiparesteSertar();
+	
+	Sertar s1 = new Sertar(5, 3, 2);
+	Sertar s2 = new Sertar(4, 2, 6);
+	Birou birou = new Birou(10, 5, 8, s1, s2);
+	birou.tiparesteBirou();
+	}
+
+}
